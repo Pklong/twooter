85623a6ed957ff1ed2353d03b997b32f6ca93962
Patrick Kovach-Long <PatrickKLong@gmail.com>
2017-02-21 12:04:52 -0500

    Fix namespacing problems in specs

--- app/helpers/api/likes_helper.rb
+++ app/helpers/api/likes_helper.rb
@@ -1,2 +1,2 @@
-module LikesHelper
+module Api::LikesHelper
 end
--- spec/models/twoot_spec.rb
+++ spec/models/twoot_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Twoot, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe Twoot, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
