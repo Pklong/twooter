c7cc841b73df4d924079366b04bd5fceb375b903
Patrick Kovach-Long <PatrickKLong@gmail.com>
2017-02-27 12:26:05 -0500

    Tweak production settings

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,14 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
 import rootReducer from '../reducers/root_reducer';
 
 const middlewares = [thunk];
 
 if (process.env.NODE_ENV !== 'production') {
-  const logger = createLogger();
-  middlewares.push(logger);
+  middlewares.push(require('redux-logger')());
 }
+
 
 export default (preloadedState = {}) => (
   createStore(
