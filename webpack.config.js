1a61173bc52fe05df536b06e97f9ad1876b0afa6
PKLong <patrickklong@gmail.com>
2018-02-25 22:10:14 -0500

    add .babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -12,14 +12,7 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env', 'react'],
-            plugins: [
-              'transform-object-rest-spread',
-              'transform-class-properties'
-            ]
-          }
+          loader: 'babel-loader'
         }
       }
     ]
